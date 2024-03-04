be34924108611a7ae0dca0b3bede0d690f60fefb
jose julian <202213865h@utea.edu.pe>
2024-03-04 10:41:05 -0500

    arrray.23

--- Array01/arraynotas.java
+++ Array01/arraynotas.java
@@ -7,7 +7,7 @@ public class arraynotas {
         System.out.println("INgrrese las notas  de  la 1 parcial");
          for (int i =0;i< notas.length;i++){
         System.out.println(" nota  del estudiante nº " + (i+1)+":");
-         notas[i]= double. parsedouble( System.console().readLine());
+        
          
          }
 }
